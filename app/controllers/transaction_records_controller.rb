5dbc69fe2842925356505d4292fdadf2d32681f5
Cristian Zambrano <criszamcham@gmail.com>
2023-08-17 11:47:31 -0400

    Add transaction amount

--- app/controllers/transaction_records_controller.rb
+++ app/controllers/transaction_records_controller.rb
@@ -1,11 +1,46 @@
 class TransactionRecordsController < ApplicationController
   def index
-    @transaction_records = TransactionRecord.all
+    if params[:tag_id]
+      @tag = Tag.find(params[:tag_id])
+      @transaction_records = @tag.transaction_records
+    else
+      @transaction_records = TransactionRecord.all
+    end
   end
 
   def show
   end
 
   def new
+    @transaction_record = TransactionRecord.new
+  end
+
+  def create
+    @transaction_record = TransactionRecord.new(transaction_record_params)
+    if @transaction_record.save
+      redirect_to tag_transaction_records_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @transaction_record = TransactionRecord.find(params[:id])
+
+    if @transaction_record.destroy
+      redirect_to tag_transaction_records_path, notice: 'Transaction was successfully deleted.'
+    else
+      flash[:alert] = 'Failed to delete transaction.'
+      render :index
+    end
+  end
+
+  def totalAmount
+  end
+
+  private
+
+  def transaction_record_params
+    params.require(:transaction_record).permit(:tag_id, :transaction_name, :amount, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'transaction_records/index'
-  get 'transaction_records/show'
-  get 'transaction_records/new'
   devise_for :users
   root 'home#index'
+
+  resources :tags do
+    resources :transaction_records, only: [:index, :new, :create, :destroy]
+  end  
 end
