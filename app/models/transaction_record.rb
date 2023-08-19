8934b535a0bcb5d9bb55019884facab30f28c7f1
Cristian Zambrano <criszamcham@gmail.com>
2023-08-18 23:50:47 -0400

    Add validations and specs to the transaction records

--- app/models/transaction_record.rb
+++ app/models/transaction_record.rb
@@ -1,4 +1,7 @@
 class TransactionRecord < ActiveRecord::Base
   belongs_to :user
   belongs_to :tag
+
+  validates :transaction_name, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
--- spec/models/transaction_record_spec.rb
+++ spec/models/transaction_record_spec.rb
@@ -10,5 +10,21 @@ RSpec.describe TransactionRecord, type: :model do
 
       expect(transaction_record).to be_valid
     end
+
+    it 'is not valid without a transaction_name' do
+      user = User.create(name: 'Test', email: 'test@example.com', password: 'password')
+      tag = user.tags.create(name: 'Groceries')
+      transaction_record = user.transaction_records.build(tag: tag, amount: 50.00)
+
+      expect(transaction_record).not_to be_valid
+    end
+
+    it 'is not valid without an amount' do
+      user = User.create(name: 'Test', email: 'test@example.com', password: 'password')
+      tag = user.tags.create(name: 'Groceries')
+      transaction_record = user.transaction_records.build(tag: tag, transaction_name: 'Grocery shopping')
+
+      expect(transaction_record).not_to be_valid
+    end
   end
 end
