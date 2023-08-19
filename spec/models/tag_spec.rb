29639f985a5468ac01e7144ee32aed8e32858d9b
Cristian Zambrano <criszamcham@gmail.com>
2023-08-19 01:09:34 -0400

    Modify styling for login page and splash screen

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe Tag, type: :model do
 
       expect(tag).to be_valid
     end
+
+    it 'is not valid without a name' do
+      user = User.create(name: 'Test', email: 'test@example.com', password: 'password')
+      tag = user.tags.build(icon: 'fa-shopping-cart')
+
+      expect(tag).not_to be_valid
+    end
   end
 end
